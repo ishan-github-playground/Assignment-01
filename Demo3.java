adf6d4199723a13e0611353828abc111b6bda6e4
Tharindu Ishan <ishanthari96@gmail.com>
2023-07-30 21:01:05 +0530

    Complete Task 4

--- Demo3.java
+++ Demo3.java
@@ -1,5 +1,10 @@
 public class Demo3 {
     public static void main(String[] args) {
-        
+        int num1=123;
+        int num2=456;
+        int num3=7890;
+        String color="\u001B[33m";
+        String reset = "\u001B[0m";
+        System.out.printf("%s(%s)%s %s-%s\n",color,num1,reset,num2,num3);
     }
 }
