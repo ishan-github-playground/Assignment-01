a03c32d493ba8805db33c9cf68700c26fa5aeee9
Tharindu Ishan <ishanthari96@gmail.com>
2023-07-30 22:24:10 +0530

    Complete Task 5

--- Demo4.java
+++ Demo4.java
@@ -1,3 +1,13 @@
 public class Demo4 {
-    
+    public static void main(String[] args) {
+        int day=28;
+        int month =12;
+        int year=2023;
+        int hour=23;
+        int minut=59;
+        int second=59;
+        String title="\u001B[33m";
+        String reset = "\u001B[0m";
+        System.out.printf("\u001B[31m%d\u001B[0m/\u001B[32m%d\u001B[0m/\u001B[33m%d\u001B[0m \u001B[34m%d\u001B[0m:\u001B[35m%d\u001B[0m:\u001B[36m%d\u001B[0m\n",month,day,year,hour,minut,second);
+    }
 }
---
+++ Demo5.java
@@ -0,0 +1,9 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int num1=978;
+        int num2=3;
+        int num3=16;
+        int num4=1484100;
+        System.out.printf("");
+    }
+}
