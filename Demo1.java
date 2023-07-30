f7dc05a9b82a2c885b87a40a3877742f443efc53
Tharindu Ishan <ishanthari96@gmail.com>
2023-07-30 20:19:28 +0530

    Complete Task 2

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,8 @@
 public class Demo1{
     public static void main(String[] args) {
         
+        double myBalance=123456.7890;
+        System.out.printf("$%,.2f\n",myBalance);
+        
     }
 }
