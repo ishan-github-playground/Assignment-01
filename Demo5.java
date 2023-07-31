58ee005a1ba44da1f8ee4aa24ff38908a80552bc
Tharindu Ishan <ishanthari96@gmail.com>
2023-07-31 08:52:11 +0530

    Complete Task 6-A

--- Demo5.java
+++ Demo5.java
@@ -4,6 +4,6 @@ public class Demo5 {
         int num2=3;
         int num3=16;
         int num4=1484100;
-        System.out.printf("");
+        System.out.printf("\u001B[33m%d\u001B[0m-\u001B[34m%d\u001B[0m-\u001B[35m%d\u001B[0m-\u001B[32m%d\u001B[0m\n",num1,num2,num3,num4);
     }
 }
---
+++ Demo6taska.java
@@ -0,0 +1,5 @@
+public class Demo6taska {
+    public static void main(String[] args) {
+        System.out.printf("[%6s50%s%6s]\n"," ","%"," ");
+    }
+}
